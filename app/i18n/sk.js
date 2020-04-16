faa79a1e91be2dbc562efcb7a4abe45adb23f7de
AdRyaneGrey <57403351+AdRyaneGrey@users.noreply.github.com>
2020-04-16 20:11:01 +0200

    preklad1

--- app/i18n/sk.js
+++ app/i18n/sk.js
@@ -1,8 +1,93 @@
 const i18n = {
-  'app.name': "Q-Tracker",
-  'selfTest': {
-    "startButton": "Spustiť test"
-  }
+    'app.name': "Sympt.io",
+    buttons: {
+        startSelfTest: "Spustiť test",
+        shareApp: 'Zdieľať aplikáciu',
+        callHelpline: 'COVID-19 INFOLINKA',
+        next: 'Ďalej',
+        skip: 'Preskočiť',
+        send: 'Poslať',
+        agree: 'Súhlasím'
+    },
+    dashboard: {
+        tabs: {
+            home: 'Domov',
+            profile: 'Profil',
+            faq: 'FAQ',
+            tracking: 'Sledovanie'
+        },
+        heatmap: {
+            tags: {
+                fever: {
+                    title: 'Horúčka'
+                },
+                dryCough: {
+                    title: 'Suchý kašeľ'
+                }
+            }
+        },
+        tracking: {
+            totalTests: 'Počet testov'
+        },
+        home: {
+            text1: 'Prvé výsledky sledovania symptómov COVID-19 v každej oblasti Slovenska',
+            explore: 'Nájdi COVID-19 prípady okolo seba',
+            header2: 'Covid Tracker',
+            header3: 'World Covid Tracker',
+            cards: {
+                newCasesLabel: 'Podozrenie na nové prípady',
+                mortalityLabel: 'Úmrtnosť'
+            }
+        }
+    },
+    nearMe: {
+        title: 'Moje okolie'
+    },
+    intro: {
+        _1: {
+            title: 'Rýchla diagnostika',
+            subtitle: 'Pomôž zabrániť šíreniu COVID-19 na Slovensku'
+        },
+        _2: {
+            title: 'Sleduj svoje okolie',
+            subtitle: 'Identifikuj vysoko rizikové oblasti a akou rýchlosťou sa vírus šíri'
+        },
+        _3: {
+            title: 'Dostávať varovania',
+            subtitle: 'Varovanie sa zobrazí, pokiaľ sa budeš nachádzať v blízkosti COVID 19 pozitívneho pacienta.'
+        }
+    },
+    testResults: {
+        safe: {
+            title: 'Si v bezpečí!',
+            text1: 'Zostaň doma a pomôž zabrániť šíreniu COVID-19.'
+        },
+        risk: {
+            title: 'Nachádzaš sa v rizikovej zóne!',
+            text1: 'Doporučujeme izoláciu seba a svojich najbližších. Kontroluj svoj zdravotný stav a teplotu 2x denne.'
+        },
+        danger: {
+            title: 'Doporučujeme testovanie!',
+            text1: 'Izoluj seba a svojich najbližších, dôsledne dodržiavaj doporučené pokyny. V prípade naliehavého stavu volaj 112.'
+        },
+
+        thankYou: 'Ďakujeme za tvoj čas.',
+        reminder: 'Kontroluj aplikáciu pravidelne a buď informovaný.',
+
+        testAgain: 'Otestovať znovu',
+        dashboard: 'Sleduj svoje okolie'
+    },
+    tos: {
+        tos: 'Podmienky používania a služby',
+        _1: {
+            title: 'Lokalizácia zariadenia',
+            text: 'Doporučujeme nastaviť zdieľanie polohy na \'Vždy\'. Nastavenie je možné neskôr zmeniť.'
+        },
+        _2: {
+            title: 'Zdieľanie dát',
+            text: 'Dáta sú zdieľané výlučne s certifikovanými zdravotnými a vedeckými pracovníkmi pre účely vedeckých štúdií.'
+        }
+    }
 };
 
 module.exports = i18n;
